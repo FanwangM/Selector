35901e225c627a3b351ca13c13b162d8990f4d59
Jun Xia <xiaj8@uci.edu>
2024-06-01 17:34:18 -0700

    Created the intro page using streamlit

---
+++ streamlit_app/pages/1_MaxMin.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from selector.methods.distance import MaxMin
+
+st.set_page_config(
+    page_title="MaxMin",
+    page_icon="assets/QC-Devs.png",
+)
+
+st.markdown("# Brute Strength - MaxMin")
+st.sidebar.header("Brute Strength - MaxMin")
